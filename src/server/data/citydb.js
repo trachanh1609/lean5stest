da3840e97652a7ff4eaf996ee093405e84d9ae7b
Vu Nguyen <trachanh@gmail.com>
2018-08-01 16:03:58 +0300

    edit get config secret

--- src/server/data/citydb.js
+++ src/server/data/citydb.js
@@ -1,35 +1,24 @@
 var tedious = require('tedious');
 
-
 var Connection = tedious.Connection;
 var Request = tedious.Request;
 
 var isProduction = process.env.NODE_ENV === 'production';
-var config = {};
-if(isProduction) {
-    config = {
-        userName: process.env.DB_USERNAME || '',
-        password: process.env.DB_PASSWORD || '',
-        server: process.env.DB_SERVER || '',
-        options: {
-            database: process.env.DB_NAME || '',
-            encrypt: true,
-            rowCollectionOnRequestCompletion: true
-        }
-    }
-} else {
-    var configuration = require('../../../config');
-    config = {
-        userName: process.env.DB_USERNAME || configuration.userName,
-        password: process.env.DB_PASSWORD || configuration.password,
-        server: process.env.DB_SERVER || configuration.server,
-        options: {
-            database: process.env.DB_NAME || configuration.name,
-            encrypt: true,
-            rowCollectionOnRequestCompletion: true
-        }
-    }    
+var configuration = {};
+
+if(!isProduction) {
+    configuration = require('../../../config');
 }
+var config = {
+    userName: process.env.DB_USERNAME || configuration.userName || 'empty',
+    password: process.env.DB_PASSWORD || configuration.password || 'empty',
+    server: process.env.DB_SERVER || configuration.server || 'empty',
+    options: {
+        database: process.env.DB_NAME || configuration.name || 'empty',
+        encrypt: true,
+        rowCollectionOnRequestCompletion: true
+    }
+} 
 
 // var createUsers = function(callback) {
 //    var connection = new Connection(config);
