c1eb559721be205c07274325144e51f7deacbfc6
Vu Nguyen <trachanh@gmail.com>
2018-08-02 10:04:19 +0300

    rearrange backend route to /api

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-const citiesRouter = require('./src/server/routes/cities');
+const indexRouter = require('./src/server/routes/');
 
 app.use(express.static('./'));
 app.use(express.static('dist'));
 
 
 
-app.use('/api/cities', citiesRouter);
+app.use('/api', indexRouter);
 
 app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
