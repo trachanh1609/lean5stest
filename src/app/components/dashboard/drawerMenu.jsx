371aa420c5efa8087b1b1fb7fe3865df8c322d72
Vu Nguyen <trachanh@gmail.com>
2018-08-20 09:41:08 +0300

    update icons, text in dashboard/left menu

--- src/app/components/dashboard/drawerMenu.jsx
+++ src/app/components/dashboard/drawerMenu.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import StarIcon from '@material-ui/icons/Star';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import ListIcon from '@material-ui/icons/List';
 import SendIcon from '@material-ui/icons/Send';
-import MailIcon from '@material-ui/icons/Mail';
+import SettingsIcon from '@material-ui/icons/Settings';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ReportIcon from '@material-ui/icons/Report';
 
@@ -14,27 +14,27 @@ export const drawerMenu = (
   <div>
     <ListItem button>
       <ListItemIcon>
-        <InboxIcon />
+        <TimelineIcon />
       </ListItemIcon>
-      <ListItemText primary="Inbox" />
+      <ListItemText primary="Raportti" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
-        <StarIcon />
+        <LocationOnIcon />
       </ListItemIcon>
-      <ListItemText primary="Starred" />
+      <ListItemText primary="Kohteet" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
-        <SendIcon />
+        <ListIcon />
       </ListItemIcon>
-      <ListItemText primary="Send mail" />
+      <ListItemText primary="Vaiheet" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
-        <DraftsIcon />
+        <SettingsIcon />
       </ListItemIcon>
-      <ListItemText primary="Drafts" />
+      <ListItemText primary="Asetukset" />
     </ListItem>
   </div>
 );
